e70c7927d9c4e85a16fd36bb6acdb3fd06b01d9f
CSH111 <seongho6022@naver.com>
2023-03-05 23:41:58 +0900

    feat: meetup 더미데이터표시

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,25 @@
 import Head from "next/head";
 import Image from "next/image";
 // import { Inter } from 'next/font/google'
-import styles from "@/styles/Home.module.css";
+import { Meetup } from "@/types";
+import MeetupList from "@/components/meetups/MeetupList";
 
 // const inter = Inter({ subsets: ['latin'] })
 
+const DUMMY_DATA: Meetup[] = [
+  {
+    id: "123",
+    title: "ddddd1",
+    address: "천안시 서북구",
+    image:
+      "https://images.unsplash.com/photo-1617854818583-09e7f077a156?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  },
+];
+
 export default function Home() {
-  return <>gdgd</>;
+  return (
+    <>
+      <MeetupList meetups={DUMMY_DATA} />
+    </>
+  );
 }
